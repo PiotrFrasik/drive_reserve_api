800c1aa25d325d44f19447a999bd1dcffdfaa5d3
PiotrFrasik <frasik.piotrek@gmail.com>
2026-04-20 20:34:58 +0200

    Add serializers for feet.

--- bookings/views.py
+++ bookings/views.py
@@ -1,3 +1,6 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from bookings.models import Booking
 
-# Create your views here.
+class BookingViewSet(viewsets.ModelViewSet):
+    queryset = Booking.objects.all()
+
