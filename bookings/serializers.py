8820673c6767dd973cc2b49148a86b15d63db618
PiotrFrasik <frasik.piotrek@gmail.com>
2026-04-22 17:48:13 +0200

    Add booking serializer adn validation

---
+++ bookings/serializers.py
@@ -0,0 +1,44 @@
+import datetime
+
+from rest_framework import serializers, status
+from bookings.models import Booking
+
+class BookingSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Booking
+        fields = '__all__'
+
+    def validate(self, data):
+        current_date = datetime.date.today()
+
+        # validate date
+        if data.get('start_date') < current_date:
+            raise serializers.ValidationError({'start_date':'Start date cannot be in the past'})
+
+        if data.get('end_date') <= data.get('start_date'):
+            raise serializers.ValidationError({'end_date':'End date cannot be after start date'})
+
+        # accessibility logic
+        conflict_bookings = Booking.objects.filter(
+            car = data['car'],
+            start_date__lt=data['end_date'],
+            end_date__gt=data['start_date']
+        ).exclude(status='CAN')
+
+        if conflict_bookings.exists():
+            raise serializers.ValidationError({'conflict_bookings':'This car already booking'})
+
+        # check car status
+        if data['car'].status != 'AB':
+            raise serializers.ValidationError({'status':'This car is inaccessible'})
+
+        #  price calculation
+        duration = data['end_date'] - data['start_date']
+        days = duration.days
+
+        if days < 0:
+            raise serializers.ValidationError({'end_date':'End date cannot be after current date'})
+
+        data['total_price'] = days * data['car'].status.daily_rate
+
+        return data
