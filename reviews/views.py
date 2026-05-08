b18d1c1eb746d99aaec0f0dd5c68a742fb63c511
PiotrFrasik <frasik.piotrek@gmail.com>
2026-05-08 17:51:54 +0200

    implement ReviewViewSet with authentication

--- reviews/views.py
+++ reviews/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from reviews.models import Review
+from reviews.serializers import ReviewSerializer
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
 
-# Create your views here.
+class ReviewViewSet(viewsets.ModelViewSet):
+    queryset = Review.objects.all()
+    serializer_class = ReviewSerializer
+    permission_classes = [IsAuthenticatedOrReadOnly]
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
